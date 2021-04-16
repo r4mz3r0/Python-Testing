e15abc99b9a0781d89f866109cb364567cd07ad2
Ramiro Gonzalez Galvez <rgonzalez69@ucmerced.edu>
2021-04-16 09:21:49 -0700

    Update HelloWorld_Test.py

--- HelloWorld_Test.py
+++ HelloWorld_Test.py
@@ -1,12 +1,13 @@
 import unittest
-class HelloWorld: 
+class HelloWorld(object): 
   def hello(self, name):
     value = "Hello Tester {0}".format(name)
-    print(value)
+    return(value)
 
 class HelloWorldTest(unittest.TestCase): 
-  def test_name_ramiro(self): 
-    val = HelloWorld("Ramiro")
+  def test_name_ramiro(self):
+    helloTest = HelloWorld() 
+    val = helloTest.hello("Ramiro")
     self.assertEqual("Hello Tester Ramiro", val); 
     
 if __name__ == "__main__": 
