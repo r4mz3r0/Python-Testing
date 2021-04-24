a769712eb41e48f8953a09a41c2dc73c4c6b9b1f
Ramiro Gonzalez Galvez <rgonzalez69@ucmerced.edu>
2021-04-24 01:11:40 -0700

    Create ZuneBug.java

---
+++ Java-Automation/ZuneBug.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+public class ZuneBug{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in); 
+    System.out.println("Enter year"); 
+    Int year = input.nextInt(); 
+  }
+  public boolean isLeapYear(int year){
+    int days = year*365
+  }
+}
